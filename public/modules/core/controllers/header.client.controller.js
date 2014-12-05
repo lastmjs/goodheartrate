00f7179f26b9fae8116f858d40faf5b2dab8605c
Jordan Last <jordan.michael.last@gmail.com>
2014-12-05 15:35:20 -0700

    got rid of horrible custom directive for toggling login/signup DOM element

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -5,6 +5,18 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+		$scope.loginOpen = false;
+		$scope.signupOpen = false;
+		
+		$scope.loginClick = function() {
+			$scope.loginOpen = !$scope.loginOpen;
+			$scope.signupOpen = false;
+		};
+		
+		$scope.signupClick = function() {
+			$scope.signupOpen = !$scope.signupOpen;
+			$scope.loginOpen = false;
+		};
 
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
